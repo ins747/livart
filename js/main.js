ad872193d15e21c493d3182df037937935ad5506
ins747 <102500307+ins747@users.noreply.github.com>
2022-05-31 17:26:07 +0900

    Add files via upload
    a

--- js/main.js
+++ js/main.js
@@ -1,15 +1,4 @@
 $(document).ready(function(){
-    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-    let vh = window.innerHeight * 0.01;
-    // Then we set the value in the --vh custom property to the root of the document
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-    // We listen to the resize event
-    window.addEventListener('resize', () => {
-    // We execute the same script as before
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-    });
-    
     $('.gnb nav ul li .sub').hide();
     var sw=0;
     // 햄버거 메뉴 버튼
